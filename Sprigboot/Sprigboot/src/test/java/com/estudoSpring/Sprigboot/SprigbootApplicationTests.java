3d3244c6b8a6772dfe2902e4ec97bcd0bf1e3c74
MarcantoniLSM <marcantonimelo1@outlook.com>
2022-08-01 17:54:27 -0300

    f

---
+++ Sprigboot/Sprigboot/src/test/java/com/estudoSpring/Sprigboot/SprigbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.estudoSpring.Sprigboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprigbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
