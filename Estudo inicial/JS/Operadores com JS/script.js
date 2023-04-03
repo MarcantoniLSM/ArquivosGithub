c8675ef79a9b06150f4d1de25b4dbba878e4a732
MarcantoniLSM <marcantonimelo1@outlook.com>
2023-04-03 11:30:05 -0300

    o

--- Estudo inicial/JS/Operadores com JS/script.js
+++ Estudo inicial/JS/Operadores com JS/script.js
@@ -1,25 +1,9 @@
-const n1 = 8;
-const n2 = 7;
-
-function igualdade(n1, n2){
-    const iguais = n1 === n2;
-    const soma = n1 + n2;
-    let similar = '';
-    let c10 = '';
-    let c20 = '';
-    if (iguais !== true) {
-        similar = 'não'; 
+let quadrado = {
+    lado: medidaLado,
+    area(medidaLado){
+        return this.lado*this.lado
     }
-    if(soma > 10){
-        c10 = 'maior';
-    }else{
-        c10 = 'menor';
-    }
-    if (soma > 20) {
-        c20 = 'maior'
-    } else {
-        c20 = 'menor'
-    }
-    return `Os números ${n1} e ${n2} ${similar} são iguais! Sua soma é ${soma}, a qual é ${c10} que 10 e ${c20} que 20. `
 }
-console.log(igualdade(n1, n2));
+let medidaLado = 4
+
+console.log(quadrado.area(medidaLado))
